01f51aeeb1d96abddfce57b83a5f871656d6d5d5
zastelaan <zooma75gmail.com>
2020-12-16 16:42:14 +0200

    Add solved task 02 to lab 01

---
+++ src/main/java/ua/edu/npu/lab01/DogRunner.java
@@ -0,0 +1,11 @@
+package ua.edu.npu.lab01;
+
+public class DogRunner {
+
+    public static void main(String[] args) {
+        Dog pug = new Dog();
+        pug.setName("Peter");
+        pug.setAge(4);
+        pug.voice();
+    }
+}
