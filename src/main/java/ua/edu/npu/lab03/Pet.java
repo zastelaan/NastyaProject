5e507e56697cae0bf0987ca372dad4117aba0c77
zastelaan <zooma75gmail.com>
2020-12-16 17:50:21 +0200

    Add solved task 02 to lab 03

---
+++ src/main/java/ua/edu/npu/lab03/Pet.java
@@ -0,0 +1,15 @@
+package ua.edu.npu.lab03;
+
+public abstract class Pet {
+	String name;
+	int age;
+
+	Pet() {
+		this("someName", 0);
+	}
+
+	Pet(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+}
